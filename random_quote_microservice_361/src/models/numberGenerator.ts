ecde8d63f7fd2258cf3df7ba5c856a9756bde38a
psykibear <casey.bellew.lowe@gmail.com>
2024-02-26 08:22:13 -0600

    Delete random_quote_microservice_361 directory

--- random_quote_microservice_361/src/models/numberGenerator.ts
+++
@@ -1,9 +0,0 @@
-export class RandomNumberGenerator {
-    /**
-     * Generates a random number within a specified range.
-     * @param min The minimum value of the range (inclusive), defaulting to 0.
-     * @param max The maximum value of the range (inclusive).
-     * @returns A random number within the specified range.
-     */
-    static generate = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min;
-}
--- random_quote_microservice_361/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import numberGeneratorController from "../controllers/numberGeneratorController";
-
-const router = express.Router();
-
-router.use("/random", numberGeneratorController);
-
-export default router;
